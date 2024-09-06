0cda8e8bd93ff922957f2227586e847810ac49d4
Sirius <alpha.canis.majoris@outlook.com>
2024-09-07 00:45:28 +0800

    update scripts

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -3,8 +3,8 @@
 ## kill currently running zookeeper processes
 PROC=$(ps -ef | grep zookeeper | grep -v grep | awk '{print $2}')
 if [ -n "$PROC" ]; then
-  echo "Killing stale ZooKeeper process(es) (ID):" $PROC
-  kill $PROC
+  echo "Killing stale ZooKeeper process(es) (ID):" "$PROC"
+  kill "$PROC"
 else
   echo "No running ZooKeeper processes to kill"
 fi
